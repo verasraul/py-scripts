5efba209f57d175a971da92b952269c1efbf1c32
Raul Veras <verasraul@gmail.com>
2024-03-19 04:26:42 -0400

    creating compare function

--- Python_Scripts/file_manipulation_Scripts/compare_files.py
+++ Python_Scripts/file_manipulation_Scripts/compare_files.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-def read_list_file():
+def read_list_file(filename):
     # Try to open the file 'list.txt' in read mode
     try:
-        with open('list.txt', 'r') as file:
+        with open(filename, 'r') as file:
             # Read the content of the file line by line
             lines = file.readlines()
             # Strip newline characters from each line and store them in a list
@@ -16,7 +16,23 @@ def read_list_file():
         print(f"Error occurred: {e}")
         return []
 
-file_content = read_list_file()
+def compare_files(fqdns_values, list_values):
+    # Print values that are in fqdns.txt but not in list.txt
+    print("Add the following:")
+    for fqdn in fqdns_values:
+        if fqdn not in list_values:
+            print(f"- {fqdn}")
 
-print(file_content)
+    # Print values that are in both files
+    print("\nDo not add the following:")
+    for fqdn in fqdns_values:
+        if fqdn in list_values:
+            print(f"- {fqdn}")
 
+# Read content of list.txt and fqdns.txt
+list_values = read_list_file('list.txt')
+fqdns_values = read_list_file('fqdns.txt')
+
+# Compare values between the two files
+if list_values and fqdns_values:
+    compare_files(list_values, fqdns_values)
