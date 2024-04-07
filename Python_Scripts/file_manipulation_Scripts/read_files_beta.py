d365b326ae560ae9fe66735bf4d9d660c0c9c3ed
Raul Veras <verasraul@gmail.com>
2024-04-07 19:20:10 -0400

    updatig comparison function

--- Python_Scripts/file_manipulation_Scripts/read_files_beta.py
+++ Python_Scripts/file_manipulation_Scripts/read_files_beta.py
@@ -25,21 +25,33 @@ def compare_files_content(fileA, fileB):
     fileA_path = search_txt_file(fileA)
     fileB_path = search_txt_file(fileB)
 
-    contentA = set(read_txt_file(fileA_path))
-    contentB = set(read_txt_file(fileB_path))
+    contentA = read_txt_file(fileA_path)
+    contentB = read_txt_file(fileB_path)
 
-    common_content = contentA.intersection(contentB)
+    if contentA is None or contentB is None:
+        print("Unable to read file content.")
+        return
 
-    if common_content:
-        print("Do not add the following content:")
-        for content in common_content:
-            print(content)
-    else:
-        print("Add the following content:")
-        for content in contentA:
-            print(content)
+    # Extracting and comparing first comma-separated strings of each line
+    lines_A = [line.strip() for line in contentA]
+    lines_B = [line.strip() for line in contentB]
+  
+
+    # Iterate over lines in A and print them based on matching or non-matching values
+    print("Do not add the following content:")
+    for line_A in lines_A:
+        first_value_A = line_A.split(',')[0].strip()
+        if first_value_A in set(line.split(',')[0].strip() for line in lines_B):
+            print(line_A)
+
+    print("\nAdd the following content:")
+    for line_A in lines_A:
+        first_value_A = line_A.split(',')[0].strip()
+        if first_value_A not in set(line.split(',')[0].strip() for line in lines_B):
+            print(line_A)
+
 
 # Example usage:
-fileA = "fileA.txt"
-fileB = "fileB.txt"
+fileA = "add_to_stock.txt"
+fileB = "movies_in_stock.txt"
 compare_files_content(fileA, fileB)
