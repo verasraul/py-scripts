7a6edf4c0b190c91562189119acd8eac641f7af9
Raul Veras <verasraul@gmail.com>
2024-10-05 12:16:46 -0400

    updating script module

--- py_csv_scripts/csv_email_script.py
+++ py_csv_scripts/csv_email_script.py
@@ -1,9 +1,9 @@
 import csv
 
 
-with open("odlr_users_csv.csv", mode='r') as csvfile:
+with open("users.csv", mode='r') as csvfile:
     readfile = (csv.reader(csvfile))
-    outputFile = open("csvOutput.csv", mode='w+')
+    outputFile = open("Output-Users.csv", mode='w+')
     writer = csv.writer(outputFile, delimiter=',',
                             quotechar=' ', quoting=csv.QUOTE_MINIMAL)
     linescounted = 0
@@ -11,10 +11,10 @@ with open("odlr_users_csv.csv", mode='r') as csvfile:
             for row in readfile:
                 if linescounted == 0:
                     print([f'{row[0]} {row[1]} {row[2]} '+"Email"])
-                    writer.writerow([f'ID, {row[1]}, {row[2]},'+"Email" ])
+                    writer.writerow([f'ID, {row[1]}, {row[2]},'+ "Email" ])
                 else:
-                    print([f'{row[0]} {row[1]} {row[2]} {row[1][0]} {row[2]}' + "@odlr.com"])
-                    writer.writerow([f'{row[0]}, {row[1]}, {row[2]}, {row[1][0]}{row[2]}' + "@odlr.com"])
+                    print([f'{row[0]} {row[1]} {row[2]} {row[1][0]} {row[2]}' + "@email.com"])
+                    writer.writerow([f'{row[0]}, {row[1]}, {row[2]}, {row[1][0]}{row[2]}' + "@email.com"])
                 linescounted +=1
                 
 
